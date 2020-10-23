a6950da851ae9af982276094e85b90c663b51d5d
MadTofu22 <stutler.tom@gmail.com>
2020-10-23 11:38:15 -0500

    added image objects to gallery.data.js

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,40 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    {   
+        id: 1, 
+        path: 'images/goat_small.jpg', 
+        description: 'Photo of a goat taken at Glacier National Park.', 
+        likes: 0
+    },
+    {
+        id: 2,
+        path: 'images/175-Togepi.png',
+        description: 'An image of the cutest pokemon',
+        likes: 0
+    },
+    {
+        id: 3,
+        path: 'images/escher.jpg',
+        description: 'Some good ole MC Escher art',
+        likes: 0
+    },
+    {
+        id: 4,
+        path: 'images/phantogram.jpeg',
+        description: 'Cover art for one of phantograms albums',
+        likes: 0
+    },
+    {
+        id: 5,
+        path: 'images/steak.jpg',
+        description: 'Tasty meats',
+        likes: 0
+    },
+    {
+        id: 6,
+        path: 'images/toast.jpg',
+        description: 'Mmmmmmm buttered toast!',
+        likes: 0
+    }
 ];
 
 module.exports = galleryItems;
