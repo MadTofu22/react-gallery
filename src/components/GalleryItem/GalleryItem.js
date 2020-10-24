4454b92487476203baf8cc92f518a946f6b7e9e2
MadTofu22 <stutler.tom@gmail.com>
2020-10-24 14:20:05 -0500

    image now changes to description when clicked

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 
 class GalleryItem extends Component {
 
+    state = {
+        imgShowing: true
+    }
+
     makePath = () => {
         return ('../../' + this.props.item.path)
     }
 
+    updateImgVisbility = () => {
+        this.setState({
+            imgShowing: !this.state.imgShowing
+        });
+    }
+
     render () {
         return (
-            <section className='galleryItem' key={this.props.item.id}>
+            <section className='galleryItem' key={this.props.item.id} onClick={this.updateImgVisbility}>
+                {this.state.imgShowing ?
                 <img src={this.makePath()} alt={this.props.item.description}/>
+                :
+                <p>{this.props.item.description}</p>
+                }
                 <br />
                 <button onClick={() => {this.props.likeImage(this.props.item.id)}}>Like</button>
             </section>
